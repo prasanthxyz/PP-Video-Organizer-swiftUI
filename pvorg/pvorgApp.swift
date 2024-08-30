f37a518de09f0fdcac280a8c7a237be1e37e0e75
Prasanth P <prasanthxyz@gmail.com>
2024-08-30 22:43:39 +0530

    Add toggle dark mode

--- pvorg/pvorgApp.swift
+++ pvorg/pvorgApp.swift
@@ -9,5 +9,19 @@ struct pvorgApp: App {
             ContentView()
                 .environmentObject(rpsData)
         }
+        .commands {
+            CommandGroup(replacing: .appSettings) {
+                Button(action: toggleDarkMode) {
+                    Text("Toggle Dark Mode")
+                }
+                .keyboardShortcut("D", modifiers: [.command, .shift])
+            }
+        }
+    }
+
+    func toggleDarkMode() {
+        let currentAppearance = NSApp.effectiveAppearance
+        let newAppearance = (currentAppearance.name == .darkAqua) ? NSAppearance.Name.aqua : NSAppearance.Name.darkAqua
+        NSApp.appearance = NSAppearance(named: newAppearance)
     }
 }
