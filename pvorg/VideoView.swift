586665ab0c18fc531843882a77b0da863daca251
Prasanth P <prasanthxyz@gmail.com>
2024-08-30 21:37:05 +0530

    Move to AVPlayer for Video playing

--- pvorg/VideoView.swift
+++ pvorg/VideoView.swift
@@ -1,83 +1,33 @@
 import SwiftUI
-import AVFoundation
-
-struct VideoPlayerView: NSViewRepresentable {
-    var player: AVPlayer
-
-    func makeNSView(context: Context) -> NSView {
-        return PlayerView(player: player)
-    }
-
-    func updateNSView(_ nsView: NSView, context: Context) {
-    }
-}
-
-class PlayerView: NSView {
-    private var playerLayer: AVPlayerLayer
-
-    init(player: AVPlayer) {
-        self.playerLayer = AVPlayerLayer(player: player)
-        super.init(frame: .zero)
-        self.wantsLayer = true
-        self.layer = playerLayer
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    override func layout() {
-        super.layout()
-        playerLayer.frame = self.bounds
-    }
-}
+import AVKit
 
 struct VideoView: View {
     @EnvironmentObject var rpsData: RpsDataViewModel
 
     @State private var player: AVPlayer?
     @State private var isLoading = true
-    @State private var currentTime: Double = 0.0
-    @State private var duration: Double = 0.0
-    @State private var playerObserver: Any?
 
     var body: some View {
         VStack {
             if isLoading {
                 Text("Loading...")
             } else if let player = player {
-                VideoPlayerView(player: player)
-                    .onChange(of: rpsData.data.isVideoPlaying) { newValue in
-                        if newValue {
+                VideoPlayer(player: player)
+                    .onChange(of: rpsData.data.isVideoPlaying) {
+                        if rpsData.data.isVideoPlaying {
                             player.play()
                         } else {
                             player.pause()
                         }
                     }
-
                 HStack {
                     Button(action: {
                         rpsData.data.isVideoPlaying.toggle()
                     }) {
-                        Image(systemName: rpsData.data.isVideoPlaying ? "pause.fill" : "play.fill")
-                            .font(.largeTitle)
+                        Image(systemName: "play.fill")
                     }
                     .keyboardShortcut(.space, modifiers: [])
-                    Slider(value: Binding(
-                        get: {
-                            currentTime
-                        },
-                        set: { newValue in
-                            currentTime = newValue
-                            let time = CMTimeMakeWithSeconds(newValue, preferredTimescale: player.currentTime().timescale)
-                            player.seek(to: time)
-                        }
-                    ), in: 0...duration)
 
-                    Text("\(formattedTime(currentTime)) / \(formattedTime(duration))")
-                }
-
-                HStack {
                     Button(action: {seek(to:0.0)}) {Text("0")}.keyboardShortcut("0", modifiers: [])
                     Button(action: {seek(to:0.1)}) {Text("1")}.keyboardShortcut("1", modifiers: [])
                     Button(action: {seek(to:0.2)}) {Text("2")}.keyboardShortcut("2", modifiers: [])
@@ -107,26 +57,18 @@ struct VideoView: View {
         let vidPath = rpsData.data.rpsConfig.vidPath
         let videoName = rpsData.data.getCurrentCombination().videoName
         let videoURL = URL(fileURLWithPath: vidPath).appendingPathComponent(videoName)
-        let player = AVPlayer(url: videoURL)
-        player.play()
-        duration = CMTimeGetSeconds(player.currentItem?.asset.duration ?? CMTime.zero)
-        playerObserver = player.addPeriodicTimeObserver(forInterval: CMTime(seconds: 1, preferredTimescale: 1), queue: .main) { time in
-            self.currentTime = CMTimeGetSeconds(time)
-        }
-        self.player = player
+        self.player = AVPlayer(url: videoURL)
+        player?.play()
         isLoading = false
     }
 
     private func unloadVideo() {
-        if let observer = playerObserver {
-            player?.removeTimeObserver(observer)
+        guard let player = self.player else {return}
+        if ((player.rate != 0) && (player.error == nil)) {
+            player.pause()
         }
-    }
-
-    private func formattedTime(_ time: Double) -> String {
-        let minutes = Int(time) / 60
-        let seconds = Int(time) % 60
-        return String(format: "%02d:%02d", minutes, seconds)
+        self.player = nil
+        isLoading = true
     }
 
     private func seek(to percentage: Double) {
